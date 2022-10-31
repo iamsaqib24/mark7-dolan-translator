888b0b10220773f9cbb2ec6ce144109cbeafbc01
Saqib <iamsaqib24@gmail.com>
2022-10-31 23:09:33 +0530

    mark7-dolan-tranlator completed

---
+++ index.js
@@ -0,0 +1,28 @@
+var textInput = document.querySelector('#text-area');
+var buttonTranslate = document.querySelector('#btn-translate');
+var outputDiv = document.querySelector('#output');
+
+var serverLink = "https://api.funtranslations.com/translate/dolan.json";
+
+function getTranslation(input) {
+    return serverLink + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    alert('There is the problem in the server!!!');
+}
+
+function eventHandler() {
+    // console.log("working fine.");
+    var inputText = textInput.value;
+
+    fetch(getTranslation(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    }).catch(errorHandler);
+
+}
+
+buttonTranslate.addEventListener('click', eventHandler);
